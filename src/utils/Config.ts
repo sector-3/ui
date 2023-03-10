2c990e0b701594ba0b394d0fbec43a72a61534f4
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-03-10 10:08:20 +0800

    Update Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,6 +2,6 @@ const chain = process.env['NEXT_PUBLIC_CHAIN'] || 'localhost'
 export const config = {
   chain: chain,
   etherscanDomain: (chain == 'mainnet') ? 'https://etherscan.io' : `https://${chain}.etherscan.io`,
-  daoFactoryAddress: (chain == 'mainnet') ? '<address>' : '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+  daoFactoryAddress: (chain == 'mainnet') ? '<address>' : '0x124101Ab1FB16390715c94A05084d9db3d3752C1'
 }
 console.log('config:', config)

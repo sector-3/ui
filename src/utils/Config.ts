07c50d4c692ec92a0b45c5eaca997f3677d6a3b6
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-03-17 13:57:10 +0800

    mainnet launch

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,7 +2,7 @@ const chain = process.env['NEXT_PUBLIC_CHAIN'] || 'localhost'
 export const config = {
   chain: chain,
   etherscanDomain: (chain == 'mainnet') ? 'https://etherscan.io' : `https://${chain}.etherscan.io`,
-  daoFactoryAddress: (chain == 'mainnet') ? '<address>' : ((chain == 'sepolia') ? '0x293aEF46130ca53868b27E3716D1DB653918d137' : '0xE6dc5d73B7E3764C42A4A71C408E4F8246f034f7'),
+  daoFactoryAddress: (chain == 'mainnet') ? '0x7d480f3a2B5F8f45CbAbe8c0833924549dd1eB12' : ((chain == 'sepolia') ? '0x293aEF46130ca53868b27E3716D1DB653918d137' : '0xE6dc5d73B7E3764C42A4A71C408E4F8246f034f7'),
   discordWebhookContributions: process.env['NEXT_PUBLIC_DISCORD_WEBHOOK_CONTRIBUTIONS'],
   discordWebhookPriorities: process.env['NEXT_PUBLIC_DISCORD_WEBHOOK_PRIORITIES'],
   discordWebhookDAOs: process.env['NEXT_PUBLIC_DISCORD_WEBHOOK_DAOS'],
